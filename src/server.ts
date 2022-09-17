bc1070ca82ca450bfc49fced04ec6883994c92c8
LeoZion <leozionsuporte@gmail.com>
2022-09-17 18:40:54 -0300

    list games with count ads

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,29 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
 
 const app = express()
+const prisma = new PrismaClient()
 
-app.get('/ads', (request, response) => {
+app.get('/games', async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true
+        }
+      }
+    }
+  })
+
+  return response.json(games);
+});
+app.post('/ads', (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+  const gameId = request.params.id;
+
   return response.json([
     { id: 1, name: 'Anúncio 1' },
     { id: 2, name: 'Anúncio 2' },
@@ -10,6 +31,12 @@ app.get('/ads', (request, response) => {
     { id: 4, name: 'Anúncio 4' },
     { id: 5, name: 'Anúncio 5' },
   ]);
-})
+});
+
+app.get('/ads/:id/discord', (request, response) => {
+  const adsId = request.params.id;
+
+  return response.json([]);
+});
 
 app.listen(3333)
