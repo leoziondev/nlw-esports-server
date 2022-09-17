aaea582dda8678e29728a99e1641332c4a88d3b5
LeoZion <leozionsuporte@gmail.com>
2022-09-17 18:53:23 -0300

    get discord by id ads

--- src/server.ts
+++ src/server.ts
@@ -50,10 +50,21 @@ app.get('/games/:id/ads', async (request, response) => {
   }));
 });
 
-app.get('/ads/:id/discord', (request, response) => {
+app.get('/ads/:id/discord', async (request, response) => {
   const adsId = request.params.id;
 
-  return response.json([]);
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord: true,
+    },
+    where: {
+      id: adsId
+    }
+  })
+
+  return response.json({
+    discord: ad.discord
+  });
 });
 
 app.listen(3333)
